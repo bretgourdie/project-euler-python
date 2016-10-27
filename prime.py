596a4646d5d1b137ee3d0f23108bdeff92626cc1
Bret Gourdie <bretgourdie@gmail.com>
2016-10-27 13:24:48 -0400

    Speed optimization
    I don't know how to do a for loop with <= checks

--- prime.py
+++ prime.py
@@ -2,9 +2,10 @@ def findPrimeFactors(n):
 	primeFactors = []
 	ii = 2
 
-	for ii in range(2, n):
+	while ii <= n:
 		while n % ii == 0:
 			primeFactors.append(ii)
 			n = n / ii
+		ii += 1
 	
 	return primeFactors
