0002527642168b86dba17f8b0e8572e1c14b8a48
Bret Gourdie <AdjectiveNounIsAdjective@gmail.com>
2016-10-29 18:00:57 -0400

    Added initial program

---
+++ p006SumSquareDifference.py
@@ -0,0 +1,17 @@
+def sumSquareDifference(limit):
+	sumOfSquares = 0
+	squareOfSums = 0
+	for num in range(1, limit+1):
+		sumOfSquares += num ** 2
+		squareOfSums += num
+	
+	squareOfSums **= 2
+	
+	return squareOfSums - sumOfSquares
+
+tests = [10]
+
+for num in tests:
+	print("The difference between the sum of the squares of the first " 
+	+ str(num)
+	+ " natural numbers and the square of the sum is " + str(sumSquareDifference(num)))
