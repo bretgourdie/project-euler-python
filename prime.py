123ea78bc5e1665ca558601d49baa814b6b05788
Bret Gourdie <bretgourdie@gmail.com>
2016-11-03 14:51:56 -0400

    Fixed prime sieve generation

--- prime.py
+++ prime.py
@@ -9,3 +9,39 @@ def findPrimeFactors(n):
 		ii += 1
 	
 	return primeFactors
+
+def getPrimes(n):
+	primeSieve = createPrimeSieve(n)
+
+	primes = convertSieveToPrimes(primeSieve)
+
+	return primes
+
+def createPrimeSieve(n):
+	primeSieve = [True] * n
+	
+	if len(primeSieve) > 0:
+		primeSieve[0] = False
+
+	for idx, isPrime in enumerate(primeSieve):
+		if isPrime:
+			curNumber = idx+1
+			curNumberSquared = curNumber ** 2
+			curNumberIndex = curNumberSquared - 1
+
+			while curNumberIndex < len(primeSieve):
+				primeSieve[curNumberIndex] = False
+				curNumberIndex += curNumber
+	
+	return primeSieve
+
+
+
+def convertSieveToPrimes(sieve):
+	primes = []
+
+	for index, isPrime in enumerate(sieve):
+		if isPrime:
+			primes.append(index+1)
+	
+	return primes
