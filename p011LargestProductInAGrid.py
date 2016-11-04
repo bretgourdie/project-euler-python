67902909072cd045574dcd81749dfa6579c4e39d
Bret Gourdie <bretgourdie@gmail.com>
2016-11-04 09:32:31 -0400

    Added skeleton program

---
+++ p011LargestProductInAGrid.py
@@ -0,0 +1,8 @@
+def greatestProduct(adjacentNumbers):
+	return -1
+
+test = [4]
+
+for num in test:
+	print("The greatest product of " + str(num) + " numbers in the same direction is "
+	+ str(greatestProduct(num)))
