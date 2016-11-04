db1e536caea2ab07d974a738a8a2ebbfea5bc71e
Bret Gourdie <bretgourdie@gmail.com>
2016-11-04 15:44:36 -0400

    Added correct algorithm

---
+++ p014LongestCollatzSequence.py
@@ -0,0 +1,25 @@
+def longestCollatz(below):
+	longestSeq = -1
+	longestTerm = 1
+
+	for ii in range(1, below+1):
+		curSeq = 1
+		curTerm = ii
+
+		while curTerm > 1:
+			if curTerm % 2 == 0:
+				curTerm //= 2
+			else:
+				curTerm = 3 * curTerm + 1
+			curSeq += 1
+		
+		if curSeq > longestSeq:
+			longestTerm = ii
+		longestSeq = max(curSeq, longestSeq)
+	
+	return longestTerm
+
+tests = [14]
+
+for num in tests:
+	print("The longest chain for numbers under " + str(num) + " is " + str(longestCollatz(num)))
