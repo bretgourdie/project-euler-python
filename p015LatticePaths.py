b988d2d445c84d207b16ffb3955454261047eeff
Bret Gourdie <bretgourdie@gmail.com>
2016-11-09 13:12:06 -0500

    Created naive algorithm; passes first test

--- p015LatticePaths.py
+++ p015LatticePaths.py
@@ -1,7 +1,14 @@
-def findPaths(length):
-	return 6
-
+def findPaths(length, width):
+	
+	if length < 0 or width < 0:
+		return 0
+	
+	if length == 0 and width == 0:
+		return 1
+	
+	return findPaths(length - 1, width) + findPaths(length, width - 1)
+	
 tests = [2]
 
 for num in tests:
-	print("A " + str(num) + "x" + str(num) + " grid has " + str(findPaths(num)) + " paths.")
+	print("A " + str(num) + "x" + str(num) + " grid has " + str(findPaths(num, num)) + " paths.")
