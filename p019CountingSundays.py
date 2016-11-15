b08d971614b42c24202c7aab8368ab375238b8dd
Bret Gourdie <bretgourdie@gmail.com>
2016-11-15 11:07:06 -0500

    Use built-in Python date functions

--- p019CountingSundays.py
+++ p019CountingSundays.py
@@ -1,38 +1,16 @@
+import datetime
+
 def getNumSundays(year):
-	daysInMonth = {
-		 0 : 31,
-		 1 : 28,
-		 2 : 31,
-		 3 : 30,
-		 4 : 31,
-		 5 : 30,
-		 6 : 31,
-		 7 : 31,
-		 8 : 30,
-		 9 : 31,
-		10 : 30,
-		11 : 31 
-	}
-
-	dayOfWeek = 1 # Jan 1 of "year" must be a Tuesday
-	monthOfYear = 0 # Start in January
 	numSundays = 0
-	leapYearDay = 0
-
-	while year < 2001:
-		if dayOfWeek == 6:
+	curDate = datetime.date(year, 1, 1)
+	while curDate.year < 2001:
+		if curDate.weekday() == 6:
 			numSundays += 1
-		
-		dayOfWeek = (dayOfWeek + daysInMonth[monthOfYear] + leapYearDay) % 7
-		monthOfYear = (monthOfYear + 1) % 12
 
-		if monthOfYear == 0:
-			year += 1
-		if monthOfYear == 1 and year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-			leapYearDay = 1
+		if curDate.month == 12:
+			curDate = datetime.date(curDate.year + 1, 1, 1)
 		else:
-			leapYearDay = 0
-
+			curDate = datetime.date(curDate.year, curDate.month + 1, 1)
 	return numSundays
 
 
