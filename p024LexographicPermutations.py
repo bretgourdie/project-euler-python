e78a207e4a78b79bb57bd07d6546163f8d4a065e
Bret Gourdie <bretgourdie@gmail.com>
2016-11-16 14:21:13 -0500

    Added problem skeleton

---
+++ p024LexographicPermutations.py
@@ -0,0 +1,8 @@
+def getLexographicPermutations(digits):
+	permutations = []
+
+	return sorted(permutations)
+
+tests = ["012"]
+for digits in tests:
+	print("The permutations of {} are {}".format(digits, getLexographicPermutations(digits)))
