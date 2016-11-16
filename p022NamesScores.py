a644e6f32c6bffec3e42951150eb9a8f59582b10
Bret Gourdie <bretgourdie@gmail.com>
2016-11-16 09:54:14 -0500

    Initial algorithm and source data

---
+++ p022NamesScores.py
@@ -0,0 +1,24 @@
+import csv
+
+def getNameScores(nameFile):
+	score = 0
+	with open(nameFile, "r") as fNameFile:
+		reader = csv.reader(fNameFile)
+		lNameFile = list(reader)
+	
+	lNameList = lNameFile[0]
+	
+	sorted(lNameList)
+
+	for ii,name in enumerate(lNameList):
+		print(name, end=": ")
+		nameScore = 0
+		for letter in name:
+			nameScore += ord(letter) - ord('@')
+		score += nameScore * (ii+1)
+	return score
+
+tests = ["p022_names.txt"]
+
+for nameFile in tests:
+	print("The total of all the name scores in {} is {}".format(nameFile, getNameScores(nameFile)))
