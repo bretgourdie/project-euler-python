2e05259ff6db0873e1e6126c79547c939122d322
Bret Gourdie <bretgourdie@gmail.com>
2016-11-16 14:38:58 -0500

    Added target test case for p024

--- p024LexographicPermutations.py
+++ p024LexographicPermutations.py
@@ -5,7 +5,7 @@ def getLexographicPermutations(digits):
 
 	return sorted(perms)
 
-tests = [("012", 4)]
+tests = [("012", 4), ("0123456789", 1000000)]
 for digits in tests:
 	print("The {}th permutation of {} is {}".format(\
 	digits[1], digits[0], getLexographicPermutations(digits[0])[digits[1]-1]))
