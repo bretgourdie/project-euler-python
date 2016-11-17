47012c022655b39e0a2725de4d346eaa6c4523d6
Bret Gourdie <bretgourdie@gmail.com>
2016-11-17 13:55:56 -0500

    Initial skeleton of problem

---
+++ p028NumberSpiralDiagonals.py
@@ -0,0 +1,6 @@
+def sumDiagonals(dimension):
+	return -1
+
+tests = [5]
+for num in tests:
+	print("The sum of the numbers on the diagonals of a {}x{} spiral is {}".format(num, num, sumDiagonals(num)))
