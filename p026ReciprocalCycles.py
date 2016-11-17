6623f6d5584429648b3c49a122aa55cce2946cc4
Bret Gourdie <bretgourdie@gmail.com>
2016-11-17 13:48:44 -0500

    Stashing current idea

--- p026ReciprocalCycles.py
+++ p026ReciprocalCycles.py
@@ -1,5 +1,36 @@
+from decimal import *
+
+class Trie:
+	def __init__(self, value=None, substrings=None):
+		self.value = value
+		self.substrings = substrings
+	
+	def __str__(self):
+		if substrings == None:
+			return self.value
+		else:
+			return substrings
+
 def findLongestCycle(toNum):
+	for num in range(2, toNum):
+		fraction = Decimal(1) / Decimal(num)
+
+		sFraction = str(fraction)
+
+		if sFraction[:2] == "0.":
+			sFraction = sFraction[2:]
+
+		print("Fraction part = {}".format(sFraction))
+		
+		trie = createTrie(sFraction)
+	
 	return -1
+
+def createTrie(fraction):
+	tFraction = fraction + "$"
+	root = Trie(value="")
+
+
 
 tests = [11]
 for num in tests:
