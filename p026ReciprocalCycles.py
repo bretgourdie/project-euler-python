142a150ca0b81948f6356b590ae009c2fd57696a
Bret Gourdie <bretgourdie@gmail.com>
2016-11-17 09:04:18 -0500

    Initial skeleton of problem

---
+++ p026ReciprocalCycles.py
@@ -0,0 +1,6 @@
+def findLongestCycle(toNum):
+	return -1
+
+tests = [11]
+for num in tests:
+	print("The value of d with the longest recurring cycle where d < {} is {}".format(num, findLongestCycle(num)))
