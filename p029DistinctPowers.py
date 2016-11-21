a3f6ec8ad6d6d35417b3b6507956a2800fff2fd0
Bret Gourdie <bretgourdie@gmail.com>
2016-11-21 13:55:30 -0500

    Added naive algorithm for distinct powers

---
+++ p029DistinctPowers.py
@@ -0,0 +1,14 @@
+def numDistinctPowers(limit):
+	numList = {}
+	for a in range(2, limit+1):
+		for b in range(2, limit+1):
+			term = a ** b
+
+			if term not in numList:
+				numList[term] = True
+	
+	return len(numList)
+
+tests = [5]
+for num in tests:
+	print("The number of distinct terms for a^b where 2 <= a <= {} and 2 <= b <= {} is {}".format(num, num, numDistinctPowers(num)))
