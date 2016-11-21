de56aad39e03fbedcfab495b771f63d96818440f
Bret Gourdie <bretgourdie@gmail.com>
2016-11-21 14:06:11 -0500

    Added naive algorithm for p030

---
+++ p030DigitFifthPowers.py
@@ -0,0 +1,21 @@
+def digitPowers(numDigits):
+	limit = 10 ** numDigits
+	begin = 10 ** (numDigits - 1)
+	sumDigitPowers = 0
+
+	for ii in range(begin, limit):
+		curSum = 0
+		curNum = ii
+		for num in range(numDigits):
+			curDigit = curNum % 10
+			curNum //= 10
+			curSum += curDigit ** numDigits
+
+		if curSum == ii:
+			sumDigitPowers += ii
+	
+	return sumDigitPowers
+
+tests = [4]
+for num in tests:
+	print("The sum of the numbers that can be written as the sum of {}th powers of their digits is {}".format(num, digitPowers(num)))
