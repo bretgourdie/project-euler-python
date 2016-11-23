c0bd0a51f0b3e7503b9d4c6d65c081454061bbfc
Bret Gourdie <bretgourdie@gmail.com>
2016-11-23 08:37:01 -0500

    Added non-working program

---
+++ p035CircularPrimes.py
@@ -0,0 +1,26 @@
+from math import ceil, log
+from prime import getPrimes
+
+def getCircularPrimes(limit):
+	primes = getPrimes(limit)
+	dPrimes = dict(zip(primes, [True] * len(primes)))
+
+	circularPrimes = []
+	for num in range(2, limit):
+		if num in dPrimes:
+			numDigits = ceil(log(num))
+			sCurNum = str(num)
+			isCircular = True
+			for ii in range(numDigits):
+				sCurNum = sCurNum[1:] + sCurNum[0]
+				iCurNum = int(sCurNum)
+				isCircular = isCircular and iCurNum in dPrimes
+
+			if isCircular:
+				circularPrimes.append(num)
+
+	return len(circularPrimes)
+
+tests = [100]
+for num in tests:
+	print("There are {} circular primes below {}".format(num, getCircularPrimes(num)))
