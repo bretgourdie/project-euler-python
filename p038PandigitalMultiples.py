de908082ba893762e13f0533e7c7b8ad77903c87
Bret Gourdie <bretgourdie@gmail.com>
2016-11-23 13:17:32 -0500

    Naive algorithm for p038

---
+++ p038PandigitalMultiples.py
@@ -0,0 +1,35 @@
+
+def checkPandigital(product):
+	nums = "0123456789"
+
+	for num in nums:
+		if int(num) > 0:
+			if product.count(num) != 1:
+				return False
+		elif int(num) == 0:
+			if product.count(num) > 0:
+				return False
+	return True
+
+def findLargestPandigital(digits):
+	largestPan = -1
+	sNum = ""
+	for num in range(1, 9+1):
+		sNum = "1"
+		n = 2
+		while len(sNum) <= 9:
+			sNum = ""
+			for ii in range(1, n+1):
+				sNum += str(num * ii)
+
+			if checkPandigital(sNum):
+				largestPan = max(largestPan, int(sNum))
+
+			n += 1
+
+	return largestPan
+
+
+tests = [9]
+for num in tests:
+	print("The largest 1 to {} pandigital {}-digit number is {}".format(num, num, findLargestPandigital(num)))
