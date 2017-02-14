df225461674b7e90553914c91ec044074621f6fc
Bret Gourdie <bretgourdie@gmail.com>
2017-02-14 13:46:06 -0500

    First attempt; does not compute all substrings

--- p043Sub-stringDivisibility.py
+++ p043Sub-stringDivisibility.py
@@ -1,12 +1,46 @@
+from prime import getPrimes
 from pandigital import checkPandigital
 
-def getSumOfSubStringPandigitalNumbers(zeroToNPandigital):
+def getSumOfSubStringPandigitalNumbers(zeroToNPandigital, substringSize):
     numDigits = zeroToNPandigital + 1
 
-    startingNumber = 10 ** zeroToNPandigital
+    #startingNumber = 10 ** zeroToNPandigital
+    startingNumber = 1023456789
     endingNumber = 10 ** numDigits
+    lPrimes = getPrimes(17)
+    lSubStringPandigitalNumbers = []
+
     for num in range(startingNumber, endingNumber):
-        isPandigital = checkPandigital(num, zeroToNPandigital)
+        isPandigital = checkPandigital(num, zeroToNPandigital, includeZero=True)
+
+        if isPandigital:
+            print("\"{}\" is pandigital".format(num))
+            primeSubStringDivisibility = True
+            for startDigit in range(substringSize - 1, numDigits - substringSize):
+                strNum = str(num)
+                lDigits = []
+                for offset in range(0, substringSize):
+                    lDigits.append(strNum[startDigit + offset])
+
+                strSubstring = ''.join(lDigits)
+                iSubstring = int(strSubstring)
+
+                primeToDivideBy = lPrimes[startDigit - substringSize + 1]
+                isDivisible = iSubstring % primeToDivideBy == 0
+                
+                primeSubStringDivisibility = primeSubStringDivisibility and isDivisible
+
+                print("Digits {} become {} which is divisible by {}? {}".format(lDigits, iSubstring, primeToDivideBy, isDivisible))
+
+            print("\"{}\" was fully divisible? {}".format(num, primeSubStringDivisibility))
+            if primeSubStringDivisibility:
+                lSubStringPandigitalNumbers.append(num)
+
+    
+    sumPandigital = sum(lSubStringPandigitalNumbers)
+
+    return sumPandigital
+
 
 
 tests = [(9,3)]
